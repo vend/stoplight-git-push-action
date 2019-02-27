d07918d4f11251b9c7ea4601d9ab67082e5c6881
Piotr Zurek <p.zurek@gmail.com>
2019-02-27 14:20:52 +1300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,11 @@ cd ..
 git clone https://$STOPLIGHT_USERNAME:$STOPLIGHT_GIT_TOKEN@git.stoplight.io/$STOPLIGHT_ORG/$STOPLIGHT_PROJECT.git
 
 echo
-echo "-- Environment variables ----------------------------------------------"
-env
-echo "-----------------------------------------------------------------------"
-
-echo
 echo "-- File system --------------------------------------------------------"
 tree
 echo "-----------------------------------------------------------------------"
 
-cp ../$STOPLIGHT_PROJECT/$HUB_FILEPATH /workspace/$HUB_FILEPATH
+copy ../$STOPLIGHT_PROJECT/$HUB_FILEPATH /workspace/$HUB_FILEPATH
 
 cd workspace
 
