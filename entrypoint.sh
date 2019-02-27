620c7f3e54020690c022fbe6151705cde763a9af
Piotr Zurek <p.zurek@gmail.com>
2019-02-28 06:53:03 +1300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,19 @@ cd ..
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name  $GIT_USER_NAME
 
-# clone the stoplight repository
+# clone the Stoplight repository
 git clone https://$STOPLIGHT_USERNAME:$STOPLIGHT_GIT_TOKEN@git.stoplight.io/$STOPLIGHT_ORG/$STOPLIGHT_PROJECT.git
 
-# go into the stoplight repo and checkout the "version" branch
+# go into the Stoplight repo and checkout the "version" branch
 cd $STOPLIGHT_PROJECT
 git checkout version/1.0
 
-# copy the hub file from workspace to stoplight repo
+# copy the hub file from workspace to the Stoplight repo
 cp  ../workspace/$HUB_FILEPATH $HUB_FILEPATH
 
-# add, commit and push the stoplight repo
+# add, commit and push the Stoplight repo
 git add -A
+
+
 git commit -m "hub file update from GitHub"
 git push
