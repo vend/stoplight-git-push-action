fad29f7b2b103c31fb0a3246dee43f773a09d8f7
Piotr Zurek <p.zurek@gmail.com>
2019-02-27 15:56:00 +1300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,11 @@ set -e
 
 cd ..
 
-git clone https://$STOPLIGHT_USERNAME:$STOPLIGHT_GIT_TOKEN@git.stoplight.io/$STOPLIGHT_ORG/$STOPLIGHT_PROJECT.git
-
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name  $GIT_USER_NAME
+
+git clone https://$STOPLIGHT_USERNAME:$STOPLIGHT_GIT_TOKEN@git.stoplight.io/$STOPLIGHT_ORG/$STOPLIGHT_PROJECT.git
+git checkout version/1.0
 
 cp  ./workspace/$HUB_FILEPATH ./$STOPLIGHT_PROJECT/$HUB_FILEPATH
 
